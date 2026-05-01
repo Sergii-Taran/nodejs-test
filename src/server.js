5a3aa3459b6f8960629feaf59b954263778ff417
Sergii Taran <serg.taran1970@gmail.com>
2026-05-01 15:37:24 +0300

    refactor(routes): move student endpoints to separate router

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
-import { Student } from './models/student.js';
+import studentsRoutes from './routes/studentsRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3001;
@@ -18,21 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.get('/students', async (req, res) => {
-  const students = await Student.find();
-  res.status(200).json(students);
-});
-
-app.get('/students/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-  const student = await Student.findById(studentId);
-
-  if (!student) {
-    return res.status(404).json({ message: 'Student not found' });
-  }
-
-  res.status(200).json(student);
-});
+app.use(studentsRoutes);
 
 app.use(notFoundHandler);
 
