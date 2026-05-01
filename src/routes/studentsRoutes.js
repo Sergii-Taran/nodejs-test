2b46ae22f420e4616882b572a44138170a0d29da
Sergii Taran <serg.taran1970@gmail.com>
2026-05-01 16:02:15 +0300

    refactor(controllers): move student route logic to controllers

--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -1,20 +1,12 @@
 import { Router } from 'express';
-import { Student } from '../models/student.js';
+import {
+  getStudents,
+  getStudentById,
+} from '../controllers/studentsController.js';
 
 const router = Router();
 
-router.get('/students', async (req, res) => {
-  const students = await Student.find();
-  res.status(200).json(students);
-});
-
-router.get('/students/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-  const student = await Student.findById(studentId);
-  if (!student) {
-    return res.status(404).json({ message: 'Student not found' });
-  }
-  res.status(200).json(student);
-});
+router.get('/students', getStudents);
+router.get('/students/:studentId', getStudentById);
 
 export default router;
