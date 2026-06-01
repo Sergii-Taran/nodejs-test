8991c74fb06be21eae315d3c3be7e9046b2e9f1b
Sergii Taran <serg.taran1970@gmail.com>
2026-06-01 19:42:33 +0300

    add reset password email template

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,9 @@ import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendEmail.js';
+
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
 
@@ -114,5 +117,33 @@ export const requestResetEmail = async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  res.status(200).json({ message: 'Password reset email sent successfully' });
+  if (!user) {
+    return res.status(200).json({
+      message: 'If this email exists, a reset link has been sent',
+    });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    });
+  } catch {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
+
+  res.status(200).json({
+    message: 'If this email exists, a reset link has been sent',
+  });
 };
