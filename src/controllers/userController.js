e26f650333a583bc35e86c54540b7366bf9f0c19
Sergii Taran <serg.taran1970@gmail.com>
2026-06-02 14:49:38 +0300

    add avatar file validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,10 @@
-export const updateUserAvatar = async (req, res) => {
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+
+export const updateUserAvatar = async (req, res, next) => {
+  if (!req.file) {
+    throw createHttpError(400, 'No file');
+  }
+
   res.status(200).json({ url: '' });
 };
